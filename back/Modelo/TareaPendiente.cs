6ca880a42fc0d90364abacadcd130d9245004868
Bolivar Mendoza - CENERGYTEK <desarrollo.externobm@cresa.ec>
2024-06-11 22:01:48 -0500

    first commit

---
+++ back/Modelo/TareaPendiente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace back
+{
+
+
+    public class TareaPendiente
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha_Creacion { get; set; }
+        public DateTime Fecha_Vencimiento { get; set; }
+        public bool Completada { get; set; }
+    }
+}
---
+++ back/Modelo/TareasPendientesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace back
+{
+    public class TareasPendientesContext : DbContext
+    {
+        public DbSet<TareaPendiente> TareasPendientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Bolivar_Mendoza;Trusted_Connection=True;");
+        }
+    }
+}
